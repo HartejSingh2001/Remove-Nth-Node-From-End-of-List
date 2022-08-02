a44445e3dd55f5a578164c92b7b17035be9d4cc6
Hartej Singh <86732914+HartejSingh2001@users.noreply.github.com>
2022-08-02 16:23:33 +0530

    Update ans.cpp

--- ans.cpp
+++ ans.cpp
@@ -2,46 +2,24 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *fast=head,*slow=head,*temp=head;
-        int count=1,count2=0;
+        int count1=1,count2=0;// Taking count1=1 is very imp
         while(temp!=NULL)
         {
             temp=temp->next;
-            count2++;
+            count2++;// size of LL
         }
-        
-        if(count2 == n)
+         if(count2 == n)// VI condition
         {
             head=head->next;
             return head;
         }
-        else
-        {
-            while(count<=n)
+        int k=count2-n;
+        while(count1<k)
         {
             fast=fast->next;
-            count++;
+            count1++;
         }
-        
-        while(fast->next != NULL)
-        {
-            if(count==n)
-            {
-                slow=head;
-                fast=fast->next;
-            }
-            else
-            {
-                slow=slow->next;
-                fast=fast->next;
-            }
-        }
-        
-        ListNode *temp2;
-        temp2=slow->next;
-        slow->next=slow->next->next;
-        return head;
-        }
-        
-        
+        fast->next=fast->next->next;
+        return head;   
     }
 };
